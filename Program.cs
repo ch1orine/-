f12fa6ef4c18d8a0ff4e542b4a40c3bd132e2828
ch1orine <75772624+ch1orine@users.noreply.github.com>
2022-01-26 17:52:41 +0800

    提交文件

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int B = 20061;
+            int N = 22332501;
+            double sh = 3455.67,sz=13780.30;
+            double A = sh * sz * 10000;
+            int num=Convert.ToInt32(Console.ReadLine());
+            int flag = 0;
+            for (int n = 0; n < 2022; n++)
+            {
+               
+              double score = A % (B + n) % B + (N + 1);
+                //Console.WriteLine(score);
+                if(score==num)
+                {
+                    flag=1;
+                }
+                
+            }
+            for (int n = 2022; n < 4044; n++)
+            {
+
+                double score = A % (B + n) % B + (N + 1);
+                if (score == num)
+                {
+                    flag = 2;
+                }
+            }
+                if (flag==1)
+               {
+                Console.WriteLine("中奖");
+               }
+
+                if(flag==2)
+               {
+                    Console.WriteLine("替补中奖");
+               }
+
+                else
+              {
+                Console.WriteLine("未中奖");
+              }
+            Console.ReadKey();
+        }
+    }
+}
